05731c809a7a3d60674c15d0edc98d2c6b6dd9b9
Yuki Ito <y.ito.works@gmail.com>
2018-09-22 01:55:59 +0900

    Fix column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180921161540) do
+ActiveRecord::Schema.define(version: 20180921165331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180921161540) do
 
   create_table "ingredients", force: :cascade do |t|
     t.integer "recipe_id"
-    t.integer "ingredient_id"
+    t.integer "food_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
