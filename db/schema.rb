22ba92cb0e9972d8dcc4e8a674562057cea6079d
Yuki Ito <y.ito.works@gmail.com>
2018-09-26 13:30:45 +0900

    Finish Add recipe-categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180925110808) do
+ActiveRecord::Schema.define(version: 20180926041824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,12 +66,18 @@ ActiveRecord::Schema.define(version: 20180925110808) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "recipe_categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "created_user_id", null: false
-    t.string "category", null: false
+    t.integer "category_id", null: false
   end
 
   create_table "storages", force: :cascade do |t|
